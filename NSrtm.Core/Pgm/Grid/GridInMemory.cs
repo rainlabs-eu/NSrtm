dfdc07de1d30b985baba2b0416b825613238d863
michal.ciszewski <michal.ciszewski@rainlabs.pl>
2015-08-26 16:51:04 +0200

    Chanbges in GridInMemory
    now gets converted data

--- NSrtm.Core/Pgm/Grid/GridInMemory.cs
+++ NSrtm.Core/Pgm/Grid/GridInMemory.cs
@@ -12,26 +12,11 @@ namespace NSrtm.Core.Pgm.Grid
     {
         private readonly IReadOnlyList<UInt16> _pgmData;
 
-        public GridInMemory([NotNull] MemoryStream pgmData, GridConstants pgmParameters)
+        public GridInMemory([NotNull] List<UInt16> pgmData, GridConstants pgmParameters)
             : base(pgmParameters)
         {
             if (pgmData == null) throw new ArgumentNullException("pgmData");
-            _pgmData = getAsUint16(pgmData)
-                .ToList()
-                .AsReadOnly();
-        }
-
-        private IEnumerable<ushort> getAsUint16(MemoryStream pgmData)
-        {
-            var data = new List<UInt16>();
-            using (EndianBinaryReader reader = new EndianBinaryReader(EndianBitConverter.Big, pgmData))
-            {
-                while (data.Count < PgmParameters.NumberOfPoints)
-                {
-                    data.Add(reader.ReadUInt16());
-                }
-            }
-            return data;
+            _pgmData = pgmData.AsReadOnly();
         }
 
         protected override double getUndulationFrom(int pointPos)
