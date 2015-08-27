a7a1a515e78858c337c639955fc08926e12c2b88
michal.ciszewski <michal.ciszewski@rainlabs.pl>
2015-08-27 13:22:50 +0200

    Temporary changes in GridFromFile

--- NSrtm.Core/Pgm/Grid/GridFromFile.cs
+++ NSrtm.Core/Pgm/Grid/GridFromFile.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using JetBrains.Annotations;
 
 namespace NSrtm.Core.Pgm.Grid
@@ -13,17 +9,18 @@ namespace NSrtm.Core.Pgm.Grid
         private readonly FileStream _file;
         private readonly Object _thisLock = new Object();
 
-        public GridFromFile([NotNull] FileStream stream, GridConstants pgmParameters) : base(pgmParameters)
+        public GridFromFile([NotNull] string path, GridConstants pgmParameters)
+            : base(pgmParameters)
         {
-            if (stream == null) throw new ArgumentNullException("stream");
-            _file = stream;
+            if (path == null) throw new ArgumentNullException("path");
+            _file = File.Open(path, FileMode.Open, FileAccess.Read, FileShare.Read);
         }
 
         protected override double getUndulationFrom(int position)
         {
             lock (_thisLock)
             {
-                _file.Seek(position + PgmParameters.PreambleLength, SeekOrigin.Begin);
+                _file.Seek(2 * position + PgmParameters.PreambleLength + 2, SeekOrigin.Begin);
                 UInt16 rawData = (UInt16)(_file.ReadByte() << 8 | _file.ReadByte());
                 return fromRawDataToUndulation(rawData);
             }
