3b832f2d3abb0612e38fee5c7b7c9d4324f918cc
michal.ciszewski <michal.ciszewski@rainlabs.pl>
2015-09-01 14:44:05 +0200

    PgmDataDescriptionExtractor uses one regex to get all the information

--- NSrtm.Core/Pgm/DataDesciption/PgmDataDescriptionExtractor.cs
+++ NSrtm.Core/Pgm/DataDesciption/PgmDataDescriptionExtractor.cs
@@ -1,17 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
-using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace NSrtm.Core.Pgm
 {
     internal static class PgmDataDescriptionExtractor
     {
-        private static readonly Regex LatLonParser = new Regex("^(?<deg>[-+0-9]+)+(?<pos>[ENSW]*)$");
+        private static readonly Regex _preambleParser =
+            new Regex(
+                @"^(P5)[\S\s]+(?=Offset)[^0-9-]+(?<offset>-?[0-9]+)[\S\s]+(?=Scale)[^0-9-]+(?<scale>-?[0-9]+\.?[0-9]+)[\S\s]+(?=Origin)[^0-9]+(?<lat>[0-9]+)[^0-9]+(?<lon>[0-9]+)[^0-9]+\S+\s(?<width>\d+)\D+(?<height>\d+)\D+(?<max>\d+)$");
 
         public static PgmDataDescription FromStream(Stream stream)
         {
@@ -23,60 +22,34 @@ namespace NSrtm.Core.Pgm
         {
             using (var reader = new StreamReader(stream, Encoding.UTF8, false, 1024, true))
             {
-                const string _pgmMagicNumber = "P5";
-                var preamble = reader.ReadLine();
-                if (preamble != _pgmMagicNumber)
-                {
-                    throw new FileFormatException(String.Format("Wrong portable gray map magic number. Actual {0}, but should be {1}.", preamble, _pgmMagicNumber));
-                }
-
+                var preamble = new StringBuilder(reader.ReadLine());
                 string lastLine;
                 do
                 {
                     lastLine = reader.ReadLine();
-                    preamble += lastLine;
-                    preamble += "\n";
+                    preamble.Append(lastLine + "\n");
                 } while (lastLine.StartsWith("#", StringComparison.CurrentCulture));
-                preamble += reader.ReadLine();
-
-                return preamble;
+                preamble.Append(reader.ReadLine());
+                return preamble.ToString();
             }
         }
 
         private static PgmDataDescription getConstatantsFromPreamble(string preamble) //TODO define magic numbers
         {
-            var unifyPreamble = preamble.Replace("#", "\n");
-            var words = unifyPreamble.Split(' ', '\n')
-                                     .ToList();
-            var offset = Convert.ToDouble(words[getParameterIndexUsingName(words, "Offset")], CultureInfo.InvariantCulture);
-            var scale = Convert.ToDouble(words[getParameterIndexUsingName(words, "Scale")], CultureInfo.InvariantCulture);
-            var latIndex = getParameterIndexUsingName(words, "Origin");
-            var lat = getLatLonValues(words, latIndex);
-            var lon = getLatLonValues(words, latIndex + 1);
-            var indexOfWidthInPreamble = words.Count - 4;
-            var width = Convert.ToInt32(words[indexOfWidthInPreamble]);
-            int indexOfHightInPreamble = words.Count - 2;
-            var hight = Convert.ToInt32(words[indexOfHightInPreamble]);
-            var maxValue = Convert.ToInt32(words.Last());
-
-            return new PgmDataDescription(offset, scale, lat, lon, width, hight, maxValue, preamble.Length);
-        }
-
-        private static int getParameterIndexUsingName(List<string> words, string parameter)
-        {
-            return words.FindIndex(v => v == parameter) + 1;
-        }
-
-        private static int getLatLonValues(List<string> words, int index)
-        {
-            var coord = (words[index]);
-            var match = LatLonParser.Match(coord);
+            var match = _preambleParser.Match(preamble);
             if (!match.Success)
             {
-                throw new ArgumentException(string.Format("Lat/long value of '{0}' is not recognised", coord));
+                throw new ArgumentException("Preamble can not be recognized");
             }
+            var offset = Convert.ToDouble(match.Groups["offset"].Value, CultureInfo.InvariantCulture);
+            var scale = Convert.ToDouble(match.Groups["scale"].Value, CultureInfo.InvariantCulture);
+            var lat = Convert.ToInt32(match.Groups["lat"].Value);
+            var lon = Convert.ToInt32(match.Groups["lon"].Value);
+            var width = Convert.ToInt32(match.Groups["width"].Value);
+            var height = Convert.ToInt32(match.Groups["height"].Value);
+            var maxValue = Convert.ToInt32(match.Groups["max"].Value);
 
-            return int.Parse(match.Groups["deg"].Value);
+            return new PgmDataDescription(offset, scale, lat, lon, width, height, maxValue, preamble.Length);
         }
     }
 }
