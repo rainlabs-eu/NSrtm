0ada29a457052126697636d0359a062265973702
michal.ciszewski <michal.ciszewski@rainlabs.pl>
2015-09-02 14:53:31 +0200

    Added InternalsVisibleTo and changed description in assemblyInfo

--- NSrtm.Core/Properties/AssemblyInfo.cs
+++ NSrtm.Core/Properties/AssemblyInfo.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("NSrtm.Core")]
-[assembly: AssemblyDescription("NSrtm.Core allows accessing SRTM data from .NET")]
+[assembly: AssemblyDescription("NSrtm.Core allows accessing data from .NET")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: InternalsVisibleTo("NSrtm.Core.xTests")]
