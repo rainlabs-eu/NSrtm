f199fa303b336ec4626961ab3f44498ded9dc480
michal.ciszewski <michal.ciszewski@rainlabs.pl>
2015-09-03 14:09:15 +0200

    Splitted huge regex into smaller ones

--- NSrtm.Core/Pgm/DataDesciption/PgmDataDescriptionExtractor.cs
+++ NSrtm.Core/Pgm/DataDesciption/PgmDataDescriptionExtractor.cs
@@ -1,17 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
+using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 
 namespace NSrtm.Core.Pgm
 {
+    internal static class PgmPreamble
+    {
+        public static readonly IReadOnlyDictionary<string, Regex> Extractor =
+            new Dictionary<string, Regex>
+            {
+                {"MagicNumber", new Regex(@"^(P5)[^$]")},
+                {"Offset", new Regex(@"[\S\s]+(?=Offset)[^\d-]+(?<offset>-?\d+(\.\d+)?)")},
+                {"Scale", new Regex(@"[\S\s]+(?=Scale)[^\d-]+(?<scale>-?\d+(\.\d+)?)")},
+                {"LatitudeOrigin", new Regex(@"[\S\s]+(?=Origin)\D+(?<lat>\d+)")},
+                {"LongitudeOrigin", new Regex(@"[\S\s]+(?=Origin)\D+\d+\D+(?<lon>\d+)")},
+                {"WidthPoints", new Regex(@"(?<width>^\d+)", RegexOptions.Multiline)},
+                {"HeightPoints", new Regex(@"^\d+\D+(?<height>\d+)", RegexOptions.Multiline)},
+                {"MaxValue", new Regex(@"^\d[\S\s]+(?<maxValue>^\d+)$", RegexOptions.Multiline)},
+            };
+    }
+
     internal static class PgmDataDescriptionExtractor
     {
-        private static readonly Regex _preambleParser =
-            new Regex(
-                @"^(P5)[\S\s]+(?=Offset)[^\d-]+(?<offset>-?\d+(\.\d+)?)[\S\s]+(?=Scale)[^\d-]+(?<scale>-?\d+(\.\d+)?)[\S\s]+(?=Origin)[^\d]+(?<lat>\d+)[^\d]+(?<lon>\d+)[^\d]+\S+\s(?<width>\d+)\D+(?<height>\d+)\D+(?<max>\d+)$");
-
         public static PgmDataDescription FromStream(Stream stream)
         {
             var preamble = extractPreambleFromStream(stream);
@@ -36,21 +50,23 @@ namespace NSrtm.Core.Pgm
 
         internal static PgmDataDescription getConstatantsFromPreamble(string preamble)
         {
-            var match = _preambleParser.Match(preamble);
-            if (!match.Success)
+            var matches = PgmPreamble.Extractor.Select(kp => kp.Value.Match(preamble))
+                                     .Zip(PgmPreamble.Extractor.Keys, (v, k) => new {k, v})
+                                     .ToDictionary(x => x.k, x => x.v);
+            foreach (var match in matches.Where(match => !match.Value.Success))
             {
-                throw new ArgumentException("Preamble can not be recognized");
+                throw new ArgumentException(String.Format("Can not extract value {0} from the preamble", match.Key));
             }
-            var offset = Convert.ToDouble(match.Groups["offset"].Value, CultureInfo.InvariantCulture);
-            var scale = Convert.ToDouble(match.Groups["scale"].Value, CultureInfo.InvariantCulture);
-            var lat = Convert.ToInt32(match.Groups["lat"].Value);
-            var lon = Convert.ToInt32(match.Groups["lon"].Value);
-            var width = Convert.ToInt32(match.Groups["width"].Value);
-            var height = Convert.ToInt32(match.Groups["height"].Value);
-            var maxValue = Convert.ToInt32(match.Groups["max"].Value);
+
+            var offset = Convert.ToDouble(matches["Offset"].Groups["offset"].Value, CultureInfo.InvariantCulture);
+            var scale = Convert.ToDouble(matches["Scale"].Groups["scale"].Value, CultureInfo.InvariantCulture);
+            var lat = Convert.ToInt32(matches["LatitudeOrigin"].Groups["lat"].Value);
+            var lon = Convert.ToInt32(matches["LongitudeOrigin"].Groups["lon"].Value);
+            var width = Convert.ToInt32(matches["WidthPoints"].Groups["width"].Value);
+            var height = Convert.ToInt32(matches["HeightPoints"].Groups["height"].Value);
+            var maxValue = Convert.ToInt32(matches["MaxValue"].Groups["maxValue"].Value);
 
             return new PgmDataDescription(offset, scale, lat, lon, width, height, maxValue, preamble.Length);
         }
-
     }
 }
