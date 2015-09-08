aa08cf16507c41e84e4c542dc4991d4e09e5ddef
Krzysztof Wesolowski <krzysztof.wesolowski@rainlabs.pl>
2015-09-08 14:12:56 +0200

    Fix versioning of release tagged builds

--- build/PatchVersions.ps1
+++ build/PatchVersions.ps1
@@ -16,7 +16,7 @@ if(($ThisCommitTag -match [regex]'(\d.\d.\d)') -and $Branch.equals("master")) {
     Write-Host "`r`nRelease for master branch`r`n"
 
     $AssemblyVersion = $ThisCommitTag.Substring(1,5) + ".$BuildCounter"
-    $AssemblyInformationalVersion = $ThisCommitTag.Substring(1) +"-$BuildCounter"
+    $AssemblyInformationalVersion = $ThisCommitTag.Substring(1)
     
     Write-Host "Tagged release ($AssemblyInformationalVersion)"
     Write-Host "##teamcity[buildNumber 'Rel-$AssemblyInformationalVersion']"
