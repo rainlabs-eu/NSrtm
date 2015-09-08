9ce13dff1e701a8c97c174bd3260563dc8e7e3fe
Krzysztof Wesolowski <krzysztof.wesolowski@rainlabs.pl>
2015-09-08 12:57:07 +0200

    Prepare for TeamCity releasing with integrated versioning

---
+++ build/Common.ps1
@@ -0,0 +1,9 @@
+#Requires -Version 3.0
+
+$TargetNuGet = Join-Path $RepoRoot "release"
+
+if($ShouldPrint) {
+    Write-Host "Script Root:`n$PSScriptRoot`n"
+    Write-Host "Repository Root:`n$RepoRoot`n"
+    Write-Host "Sln Root:`n$SlnRoot`n"
+}
---
+++ build/NuGet.Pack.ps1
@@ -0,0 +1,9 @@
+. .\Common.ps1
+
+if(Test-Path $TargetNuGet) {
+	Remove-Item $TargetNuGet -Force -Recurse
+    md $TargetNuGet
+}
+
+
+nuget pack $SlnRoot/NSrtm.Core/NSrtm.Core.csproj -OutputDirectory $TargetNuGet -Prop Configuration=Release -Build -IncludeReferencedProjects
