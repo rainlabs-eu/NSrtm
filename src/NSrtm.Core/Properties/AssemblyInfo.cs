d86ab58f1d7e7756f1326e832576880c8336cebe
Michal Ciszewski <michal.ciszewski@rainlabs.pl>
2015-09-09 15:38:31 +0200

    Move CLSCompilant attribute from global assembly to local

--- src/NSrtm.Core/Properties/AssemblyInfo.cs
+++ src/NSrtm.Core/Properties/AssemblyInfo.cs
@@ -6,3 +6,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("NSrtm.Core")]
 [assembly: AssemblyDescription("NSrtm.Core allows accessing digital terrain elevation data from .NET")]
 [assembly: InternalsVisibleTo("NSrtm.Core.xTests")]
+[assembly: CLSCompliant(true)]
