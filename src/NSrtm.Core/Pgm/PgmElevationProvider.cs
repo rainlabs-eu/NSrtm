a69b769c04fdf60e463d5d92b52d609641b46055
Michal Ciszewski <michal.ciszewski@rainlabs.pl>
2015-09-23 15:43:03 +0200

    Get elevation using interpolation

--- src/NSrtm.Core/Pgm/PgmElevationProvider.cs
+++ src/NSrtm.Core/Pgm/PgmElevationProvider.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Concurrent;
+using System.Linq;
 using JetBrains.Annotations;
 using NSrtm.Core.BicubicInterpolation;
 using NSrtm.Core.Pgm.GeoidUndulationGrid;
@@ -10,13 +11,50 @@ namespace NSrtm.Core.Pgm
     {
         private readonly IPgmGeoidUndulationGrid _discreteSurface;
 
-        private readonly ConcurrentDictionary<PgmCellCords, BivariatePolynomial> _continuousSurface =
-            new ConcurrentDictionary<PgmCellCords, BivariatePolynomial>();
+        private readonly ConcurrentDictionary<PgmCellCoords, BivariatePolynomial> _continuousSurface =
+            new ConcurrentDictionary<PgmCellCoords, BivariatePolynomial>();
 
         public PgmElevationProvider([NotNull] IPgmGeoidUndulationGrid discreteSurface)
         {
             if (discreteSurface == null) throw new ArgumentNullException("discreteSurface");
             _discreteSurface = discreteSurface;
+        }
+
+        internal BivariatePolynomial interpolateCellSurface(PgmCellCoords pgmCellCoords)
+        {
+            var latitudes = Enumerable.Range(-1, 4)
+                                      .Select(step => pgmCellCoords.Lon + step * _discreteSurface.PgmParameters.LongitudeIncrementDegrees);
+            var longitudes = Enumerable.Range(-1, 4)
+                                       .Select(step => pgmCellCoords.Lat + step * _discreteSurface.PgmParameters.LatitudeIncrementDegrees);
+            var coordinates = latitudes.SelectMany(lat => longitudes.Select(lon => normalizeCoords(lat,lon))).ToList();
+            var data = coordinates.Select(coor => _discreteSurface.GetUndulation(coor.Lat, coor.Lon));
+            var dataFormated = data.Select((c, i) => new { Index = i, value = c })
+                            .GroupBy(p => p.Index / 4)
+                            .Select(c => c.Select(v => v.value)
+                                          .ToList())
+                            .ToList();
+            var spline = BicubicCalculator.GetSpline(dataFormated, _discreteSurface.PgmParameters.LatitudeIncrementDegrees);
+            return spline;
+        }
+
+        private static PgmCellCoords normalizeCoords(double lat, double lon)
+        {
+            var pushedLon = lon;
+            if (lat > 90 || lat < -90)
+            {
+                pushedLon += 180;
+            }
+            return new PgmCellCoords(normalizeLatitude(lat), normalizeLongitude(pushedLon));
+        }
+
+        private static double normalizeLongitude(double lon)
+        {
+            return (360 + (lon % 360)) % 360;
+        }
+
+        private static double normalizeLatitude(double lat)
+        {
+            return Math.Atan(Math.Sin(lat) / Math.Abs(Math.Cos(lat)));
         }
 
         public double GetElevation(double latitude, double longitude)
@@ -26,9 +64,7 @@ namespace NSrtm.Core.Pgm
             return interpolatedCell.Evaluate(latitude, longitude);
         }
 
-        private BivariatePolynomial interpolateCellSurface(PgmCellCords pgmCellCords)
-        {
-            throw new NotImplementedException();
-        }
+        public Level ElevationBase { get { return _discreteSurface.PgmParameters.Level; } }
+        public Level ElevationTarget { get { return Level.EllipsoidWgs84; } }
     }
 }
