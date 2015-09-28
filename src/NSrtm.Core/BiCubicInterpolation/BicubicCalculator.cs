5f028924d444c5ffb613ab60cb0625eb2fdffb7f
Michal Ciszewski <michal.ciszewski@rainlabs.pl>
2015-09-28 13:22:29 +0200

    Add missing namespace to BicubicCalculator

--- src/NSrtm.Core/BiCubicInterpolation/BicubicCalculator.cs
+++ src/NSrtm.Core/BiCubicInterpolation/BicubicCalculator.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using JetBrains.Annotations;
+using NSrtm.Core.BicubicInterpolation.NSrtm.Core.BicubicInterpolation;
 
 namespace NSrtm.Core.BicubicInterpolation
 {
