760e6f942a072f0c435efb8e19bc33cbf23b28b9
Michal Ciszewski <michal.ciszewski@rainlabs.pl>
2015-09-28 13:13:27 +0200

    Bivariate Polynomial code clean up

--- src/NSrtm.Core/BiCubicInterpolation/BivariatePolynomial.cs
+++ src/NSrtm.Core/BiCubicInterpolation/BivariatePolynomial.cs
@@ -5,15 +5,16 @@ using JetBrains.Annotations;
 
 namespace NSrtm.Core.BicubicInterpolation
 {
-    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1815:OverrideEqualsAndOperatorEqualsOnValueTypes", Justification = "This value type would never be compared")]
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1815:OverrideEqualsAndOperatorEqualsOnValueTypes",
+        Justification = "This value type would never be compared")]
     public struct BivariatePolynomial
     {
         /// <summary>
-        /// https://en.wikipedia.org/wiki/Bicubic_interpolation alpha matrix
-        /// a33 a32 a31 a30
-        /// a23 a22 a21 a20
-        /// a13 a12 a11 a10
-        /// a03 a02 a01 a00
+        ///     https://en.wikipedia.org/wiki/Bicubic_interpolation alpha matrix
+        ///     a33 a32 a31 a30
+        ///     a23 a22 a21 a20
+        ///     a13 a12 a11 a10
+        ///     a03 a02 a01 a00
         /// </summary>
         private readonly double[][] _reversedCoefficients;
 
@@ -22,10 +23,12 @@ namespace NSrtm.Core.BicubicInterpolation
             if (coefficients == null) throw new ArgumentNullException("coefficients");
             if (coefficients.Count != 16) throw new ArgumentException("Bivariate polynomial use 16 coefficients.", "coefficients");
             _reversedCoefficients = coefficients.Select((c, i) => new {Index = i, value = c})
-                                        .GroupBy(p => p.Index / 4)
-                                        .Select(c => c.Select(v => v.value)
-                                                      .Reverse().ToArray())
-                                        .Reverse().ToArray();
+                                                .GroupBy(p => p.Index / 4)
+                                                .Select(c => c.Select(v => v.value)
+                                                              .Reverse()
+                                                              .ToArray())
+                                                .Reverse()
+                                                .ToArray();
         }
 
         public double Evaluate(double x, double y)
@@ -33,7 +36,7 @@ namespace NSrtm.Core.BicubicInterpolation
             var hornersResultsTermsY = new List<double>();
             foreach (var revCoeffVector in _reversedCoefficients)
             {
-                   hornersResultsTermsY.Add(revCoeffVector.UseHornerScheme(y));
+                hornersResultsTermsY.Add(revCoeffVector.UseHornerScheme(y));
             }
             var hornerResultTermsX = hornersResultsTermsY.UseHornerScheme(x);
             return hornerResultTermsX;
